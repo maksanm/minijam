ad5a143e05ff51980c6347a3a297bc633795b79e
scanax2 <scanax2@gmail.com>
2020-09-18 18:31:26 +0200

    Работаю над скриптом для битвы.

--- game/Assets/SquadController.cs
+++ game/Assets/SquadController.cs
@@ -5,6 +5,8 @@ using Pathfinding;
 
 public class SquadController : MonoBehaviour
 {
+    public float visionRange;
+    public float engageRange;
 
     public float speed = 200f;
     public float nextWaypointDistance = 0.5f;
@@ -27,6 +29,17 @@ public class SquadController : MonoBehaviour
 
     private bool isEngage;
 
+
+    private State currentState;
+    private enum State
+    {
+        Waiting,
+        Defend,
+        Moving,
+        Engage,
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,37 +60,44 @@ public class SquadController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case State.Waiting:
+                UpdateWaitingState();
+                break;
+            case State.Defend:
+                UpdateDefendState();
+                break;
+            case State.Moving:
+                UpdateMovingState();
+                break;
+            case State.Engage:
+                UpdateEngageState();
+                break;
+        }
+
+   
+
+    
+    }
+
+    void UpdateWaitingState()
+    {
         if (Input.GetMouseButtonDown(1))
         {
-            Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-           
-            Grid grid;
-            grid = FindObjectOfType<Grid>();
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
-            Vector3Int position = grid.WorldToCell(worldPoint);
-
-            Debug.Log(grid.CellToWorld(position).x + 1.65f +" "+ grid.CellToWorld(position).y + 1.65f);
-
-            target = new Vector2(grid.CellToWorld(position).x+1.65f, grid.CellToWorld(position).y+1.65f);
-
-            SetAnim(1);
-            seeker.StartPath(transform.position, target, OnPathComplete);
+            Move();
         }
-
     }
-
-    void SetAnim(int AnimState)
+    void UpdateDefendState()
     {
-        for (int i = 0; i < transform.childCount; i++)
+    }
+    void UpdateMovingState()
+    {
+        if (Input.GetMouseButtonDown(1))
         {
-            transform.GetChild(i).GetComponent<Animator>().SetInteger("MoveState", AnimState);
+            Move();
         }
-    }
 
-    void FixedUpdate()
-    {
         if (path == null)
         {
             return;
@@ -85,6 +105,9 @@ public class SquadController : MonoBehaviour
 
         if (currentWaypoint >= path.vectorPath.Count)
         {
+            currentState = State.Waiting;
+            Debug.Log(currentState);
+
             SetAnim(0);
             reachedEndOfPath = true;
             return;
@@ -99,7 +122,7 @@ public class SquadController : MonoBehaviour
         Vector2 force = direction * speed * Time.deltaTime;
 
         Rotation = Mathf.Atan2(path.vectorPath[currentWaypoint].y - transform.position.y, path.vectorPath[currentWaypoint].x - transform.position.x);
-        
+
         transform.rotation = Quaternion.Euler(0, 0, Rotation * Mathf.Rad2Deg);
 
 
@@ -111,6 +134,50 @@ public class SquadController : MonoBehaviour
         {
             currentWaypoint++;
         }
+    }
+    void UpdateEngageState()
+    {
+    }
+
+    private void Move()
+    {
+        // Changing current state to Moving
+        currentState = State.Moving;
+        Debug.Log(currentState);
+
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        Grid grid;
+        grid = FindObjectOfType<Grid>();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        Vector3Int position = grid.WorldToCell(worldPoint);
+
+        Debug.Log(grid.CellToWorld(position).x + 1.65f + " " + grid.CellToWorld(position).y + 1.65f);
+
+        target = new Vector2(grid.CellToWorld(position).x + 1.65f, grid.CellToWorld(position).y + 1.65f);
+
+        SetAnim(1);
+        seeker.StartPath(transform.position, target, OnPathComplete);
+    }
+
+    //private void SwitchState(State state)
+    //{
+    //    currentState = state;
+    //}
+
+    void SetAnim(int AnimState)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Animator>().SetInteger("MoveState", AnimState);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
 
     }
 
