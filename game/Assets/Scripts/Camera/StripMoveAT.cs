de73c53df4469df690ab7e633ba27ce0f4391a83
Maks <forti107@gmail.com>
2020-09-18 18:12:58 +0300

    Починил полоски(вроде)

---
+++ game/Assets/Scripts/Camera/StripMoveAT.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StripMoveAT : MonoBehaviour
+{
+	private Transform trans;
+    void Start()
+    {
+    	trans = GetComponentInParent<Transform>();
+    	trans.position = new Vector2(trans.position.x, trans.position.y);
+    }
+
+    void Update()
+    {
+    	transform.position =  new Vector2(12.921f, trans.position.y);
+    }
+}
