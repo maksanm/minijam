aaba8a81f3a86eecf840d0ab0fecb08b0ffa05f2
Maks <forti107@gmail.com>
2020-09-18 14:29:35 +0300

    Скрипт для камеры

--- game/Assets/Scripts/Camera/CameraScript.cs
+++ game/Assets/Scripts/Camera/CameraScript.cs
@@ -4,24 +4,29 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
         public float krai = 10f;
         public float speed = 10f;
-       
+        private Vector3 startPos;
         void Start () 
         {
-               
+           startPos = transform.position;
         }
        
-        void Update () {
-                if(Input.mousePosition.x < krai){
-                        transform.position -= new Vector3 (speed,0,0) * Time.deltaTime;
-                }
-                if(Input.mousePosition.x > Screen.width - krai){
-                        transform.position += new Vector3 (speed,0,0) * Time.deltaTime;
-                }
-                if(Input.mousePosition.y < krai){
-                        transform.position -= new Vector3 (0,0,speed) * Time.deltaTime;
-                }
-                if(Input.mousePosition.y > Screen.height - krai){
-                        transform.position += new Vector3 (0,0,speed) * Time.deltaTime;
-                }
+        void Update () 
+        {
+        	if(Input.GetKeyDown(KeyCode.Space))
+        	{
+        		transform.position = startPos;
+        	}
+            if(Input.mousePosition.x < krai){
+                    transform.position -= new Vector3 (speed,0,0) * Time.deltaTime;
+            }
+            if(Input.mousePosition.x > Screen.width - krai){
+                    transform.position += new Vector3 (speed,0,0) * Time.deltaTime;
+            }
+            if(Input.mousePosition.y < krai){
+                    transform.position -= new Vector3 (0,speed,0) * Time.deltaTime;
+            }
+            if(Input.mousePosition.y > Screen.height - krai){
+                    transform.position += new Vector3 (0,speed,0) * Time.deltaTime;
+            }
         }
 }
