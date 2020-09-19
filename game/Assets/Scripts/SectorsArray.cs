af045af7a2347c4383035bb7cba8a20bc544c3f7
Maks <forti107@gmail.com>
2020-09-19 20:59:10 +0300

    координаты центров

---
+++ game/Assets/Scripts/SectorsArray.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectorsArray : MonoBehaviour
+{
+	Vector2[,] sectors = new Vector2[20, 20];
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+        	for (int j = 0; j < 20; j++)
+        	{
+        		sectors[i, j] = new Vector2(-20.67f+64.55f/40+64.55f*i/20,-62.65f+64.55f/40+64.55f*j/20);
+        	}
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
