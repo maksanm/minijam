13256e7e843733d6beb9ee90e25f031a46371810
Maks <forti107@gmail.com>
2020-09-19 22:08:39 +0300

    Зум камеры, подогнал всё по красоте

--- game/Assets/Scripts/Camera/CameraScript.cs
+++ game/Assets/Scripts/Camera/CameraScript.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
         public float krai = 10f;
         public float speed = 10f;
+        public float maxScale = 8;
+        public float minScale = 4;
         private Vector3 startPos;
+        private float mw;
+        public float startScale = 6;
+        public float scaleSpeed = 1f;
         float height;
  		float width;
         Camera cam;
@@ -12,27 +17,37 @@ public class CameraScript : MonoBehaviour {
         {
         	cam = Camera.main;
             startPos = transform.position;
-            height = 2f * cam.orthographicSize;
-            width = height * cam.aspect;
+            cam.orthographicSize = startScale;
         }
        
         void Update () 
         {
+            height = 2f * cam.orthographicSize;
+            width = height * cam.aspect;
+            mw = Input.GetAxis("Mouse ScrollWheel");
         	if(Input.GetKeyDown(KeyCode.Space))
         	{
         		transform.position = startPos;
         	}
-            if(Input.mousePosition.x < krai && transform.position.x > -7.3f){
+            if(Input.mousePosition.x < krai && transform.position.x - 1 - width/2 > -20.67f){
                     transform.position -= new Vector3 (speed,0,0) * Time.deltaTime;
             }
-            if(Input.mousePosition.x > Screen.width - krai && transform.position.x < 33.11f){
+            if(Input.mousePosition.x > Screen.width - krai && transform.position.x - 1 + width/2 < 43.88f){
                     transform.position += new Vector3 (speed,0,0) * Time.deltaTime;
             }
-            if(Input.mousePosition.y < krai && transform.position.y > -54.55f){
+            if(Input.mousePosition.y < krai && transform.position.y - 1 - height/2 > -62.85f){
                     transform.position -= new Vector3 (0,speed,0) * Time.deltaTime;
             }
-            if(Input.mousePosition.y > Screen.height - krai && transform.position.y < -3.57f){
+            if(Input.mousePosition.y > Screen.height - krai && transform.position.y - 1 + height/2 < 1.7f){
                     transform.position += new Vector3 (0,speed,0) * Time.deltaTime;
+            }
+            if (mw < -0.1 && cam.orthographicSize <= maxScale)
+            {
+                cam.orthographicSize += scaleSpeed;
+            }
+            if (mw > 0.1 && cam.orthographicSize >= minScale)
+            {
+                cam.orthographicSize -= scaleSpeed;
             }
         }
 }
--- game/Assets/Scripts/Camera/StripMoveAT.cs
+++ game/Assets/Scripts/Camera/StripMoveAT.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class StripMoveAT : MonoBehaviour
 {
+	Camera cam;
     void Start()
     {
-        transform.localPosition = new Vector3(0, 6.14f, 0);
+    	cam = Camera.main;
     }
 
     void Update()
     {
+    	transform.localPosition = new Vector3(0, 6.14f*cam.orthographicSize/6.76f, 0);
     	transform.position =  new Vector2(12.921f, transform.position.y);
     }
 }
