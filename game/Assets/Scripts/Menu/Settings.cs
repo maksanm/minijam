68e48ccee0572d5b5175e99330974eadd8393912
Maks <forti107@gmail.com>
2020-09-20 18:47:31 +0300

    Добавил меню, настройки звука и ПРОСТО НЕВЪЕБЕННЫЙ СЛАЙДЕР БОЖЕ ОН ТАКОЙ ОХУЕННЫЙ АВВАВАВАВА

---
+++ game/Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+	public AudioMixer am;
+    public float sliderValue;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void AudioVolume()
+    {
+        sliderValue = GameObject.Find("Slider").GetComponent<Slider>().value;
+        Debug.Log(sliderValue);
+        am.SetFloat("masterVolume", sliderValue);
+    }
+}
